9129ba3b5224607899a8361bd929ee74407b87b0
leo polanco <leo.polanco.n@gmail.com>
2021-04-01 20:54:31 -0400

    improvements to search bar

--- src/components/layout/Navbar/SearchBar.js
+++ src/components/layout/Navbar/SearchBar.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Select, { createFilter } from 'react-select'
+import { stocks } from '../../../assets/stocks'
+import {optimizeSelect} from './SelectHelper.tsx'
 
 const SearchBar = () => {
+  const [selectedOption, setSelectedOption] = useState('')
+
+  const handleChange = (e) => {
+    console.log(e.value)
+    setSelectedOption(e.value)
+  }
   return (
-    <div>
-      Search
-    </div>
+    <Select
+      value={selectedOption}
+      onChange={handleChange}
+      options={stocks}
+      filterOption={createFilter({ ignoreAccents: false })}
+      components={optimizeSelect.components}
+    />
   )
 }
 
--- src/components/layout/Navbar/index.js
+++ src/components/layout/Navbar/index.js
@@ -1,64 +1,10 @@
-import axios from 'axios'
-import React, { useState, useEffect } from 'react'
-import iex from '../../../api/iex'
+import React from 'react'
+import SearchBar from './SearchBar'
 
 const Navbar = () => {
-  const [stock, setStock] = useState({
-    name: '',
-    symbol: '',
-    price: 0
-  })
-  const { name, symbol, price } = stock
-  const [search, setSearch] = useState('')
-  const [result, setResult] = useState({})
-  const searchApi = async (searchQ) => {
-    searchQ = searchQ.replace(/\s/g, '')
-    try {
-      const {data, data: {symbol, latestPrice }} = await axios.get(`https://sandbox.iexapis.com/stable/stock/${searchQ}/quote`,{
-
-      params:{
-        token: process.env.REACT_APP_IEX_KEY
-      }
-      })
-      setResult(data)
-      console.log(latestPrice)
-      console.log(symbol)
-      console.log(data)
-    } catch (err) {
-      console.error(err)
-    }
-  }
-
-  useEffect(() => {
-    if (result) {
-      setStock({
-        name: result.companyName,
-        symbol: result.symbol,
-        price: result.latestPrice
-      })
-    }
-  }, [result])
-
-  const onSubmit = (e) => {
-    e.preventDefault()
-    searchApi(search)
-  }
   return (
     <>
-      <div>
-        <form onSubmit={onSubmit}>
-          <input
-            type='text'
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <button type='submit'>Search</button>
-        </form>
-        <h1>
-          Stock: {name} - {symbol}
-        </h1>
-        <h3>Price: {price}</h3>
-      </div>
+      <SearchBar/>
     </>
   )
 }
